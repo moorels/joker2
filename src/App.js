c43a18ed185a4f66c22650a4aca50d1003fac449
moorels <sales@micropls.com>
2021-07-02 11:57:40 +1000

    ZAS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React,{useState, useRef} from 'react'
 import './App.css';
+import useRandomJoke from './useRandomJoke';
 
 function App() {
+
+ const firstNameRef = useRef(null);
+ const lastNameRef = useRef(null);
+
+
+  
+const [firstName, setFirstName]  = useState ('');
+const [lastName, setLastName]  = useState ('');
+
+const joke = useRandomJoke(firstName, lastName)
+
+
+const generateJoke = (e) => {
+  e.preventDefault();
+
+
+  setFirstName(firstNameRef.current.value);
+  setLastName(lastNameRef.current.value);
+ 
+}
+
+const reset = () => {
+  setFirstName('');
+  setLastName('');
+}
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <h1 style={{color:"yellow" , fontSize: "70px"}}> -JOKER-</h1>
+        
+<form>
+  <input placeholder="First Name" ref={firstNameRef} />
+  <input placeholder="Last Name"  ref={lastNameRef}/>
+  <button style ={{color:"red" , fontSize: "30px"}} onClick={generateJoke}>JOKEBOOK</button>
+  <button style ={{color:"red" , fontSize: "30px"}} onClick={reset}>RESET</button>
+</form>
+
+
+      
+        <h2>{joke}</h2>
+      </center>
     </div>
   );
 }
